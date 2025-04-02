211b15ce921a4b289d85dc48f5a742e8384143b0
frank-32one <frank.32one@gmail.com>
2025-04-03 02:31:22 +0530

    Updates

---
+++ assets/browser-3RxrvF2z.js
@@ -0,0 +1,1 @@
+import{g as e}from"./index-BaW1cT8h.js";const o=e((function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")})),t=Object.freeze(Object.defineProperty({__proto__:null,default:o},Symbol.toStringTag,{value:"Module"}));export{t as b};
